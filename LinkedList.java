ba45801a3a4c8affb665ee50cfa17e015fe99599
Your Name <roshikanayanadhara@gmail.com>
2022-09-18 07:50:13 +0530

    first commit

---
+++ LinkedList.java
@@ -0,0 +1,35 @@
+class LinkedList{
+	Node head = null; //Head of the list
+	
+	//Node class
+	static class Node{
+		Node head,next;
+		int data;
+		Node(){
+			next=head = null;
+		}
+		Node(int data){
+			this.data = data;
+			head = next = null;
+		}
+	}//End of Node class
+		
+	void printList(){
+		Node temp = head;
+		System.out.print("LinkedList is : ");
+		while(temp!=null){
+			System.out.print(temp.data+" | ");
+			temp = temp.next;
+		}System.out.println();
+	}
+
+	public static void main(String args[]){
+		LinkedList A = new LinkedList();
+		A.head = new Node(10);
+		Node second_Node = new Node(20);
+		Node third_Node = new Node(30);
+		A.head.next = second_Node;
+		second_Node.next = third_Node;
+		A.printList();
+	}
+}
