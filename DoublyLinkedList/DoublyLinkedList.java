b83ff797da27e3c916a0a3a204ce8dba75fe6da6
Your Name <roshikanayanadhara@gmail.com>
2022-09-19 14:09:28 +0530

    add Node

--- DoublyLinkedList/DoublyLinkedList.java
+++ DoublyLinkedList/DoublyLinkedList.java
@@ -9,7 +9,35 @@ public class DoublyLinkedList {
             prev = null;
         }
     }
-    
+    boolean isEmpty(){
+        return head == null;
+    }
+    void addNode(int value){
+        Node newNode = new Node(value);
+        if(isEmpty()){
+            head = newNode;
+            tail = newNode;
+        }else{
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+
+        }
+
+    }
+
+    void printList(String message){
+        Node temp = head;
+        System.out.println(message);
+        if(head==null){
+			System.out.println("List is Empty!!!");
+		}
+        while(temp!=null){
+            System.out.print(temp.data+" | ");
+            temp = temp.next;
+        }System.out.println();
+    }
+
     public static void main(String args[]){
         DoublyLinkedList D = new DoublyLinkedList();
         
