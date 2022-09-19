9a967bc8080c8fd30f8420d12055635edeac7597
Your Name <roshikanayanadhara@gmail.com>
2022-09-19 20:17:07 +0530

    stack creation

---
+++ StackAsArray/StackAsArray.java
@@ -0,0 +1,16 @@
+//package StackAsArray;
+class Stack{
+    int top,size;
+    int SArray[] = new int[size];
+    Stack(int size){
+        top = -1;
+        this.size = size;
+    }
+}
+public class StackAsArray {
+    public static void main(String args[])
+    {
+        Stack s = new Stack(5);
+        
+    }
+}
