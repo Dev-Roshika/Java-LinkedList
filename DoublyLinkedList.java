d9553d76b866117d93217caecbc88ffc98dd9e3b
Your Name <roshikanayanadhara@gmail.com>
2022-09-19 14:04:00 +0530

    DoublyLinked list

---
+++ DoublyLinkedList.java
@@ -0,0 +1,17 @@
+public class DoublyLinkedList {
+    Node head,tail;
+    static class Node{
+        Node next,prev;
+        int data;
+        Node(int data){
+            this.data = data;
+            next = null;
+            prev = null;
+        }
+    }
+    
+    public static void main(String args[]){
+        DoublyLinkedList D = new DoublyLinkedList();
+        
+    }
+}
